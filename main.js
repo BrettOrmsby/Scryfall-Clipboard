e655637879600e5f58fd5f2e840dabbd747bc435
BrettOrmsby <brettwormsby@outlook.com>
2023-06-07 14:19:35 -0400

    feat: add functionality for copy and erase buttons

--- main.js
+++ main.js
@@ -48,10 +48,22 @@ clipBoardBar.classList.add("clipboard-bar");
 const copyButton = document.createElement("button");
 copyButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-paperclip"><path class="no-fill" d="m21.44 11.05-9.19 9.19a6 6 0 0 1-8.49-8.49l8.57-8.57A4 4 0 1 1 18 8.84l-8.59 8.57a2 2 0 0 1-2.83-2.83l8.49-8.48"></path></svg><b>Copy</b>`;
 copyButton.classList.add("button-n");
+copyButton.onclick = () => {
+  const clipBoard = getClipBoard();
+  const clipBoardWithPrefixNumbers = clipBoard.map((e) => "1 " + e).join("\n");
+  navigator.clipboard.writeText(clipBoardWithPrefixNumbers);
+};
 
 const clearButton = document.createElement("button");
 clearButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-eraser"><path class="no-fill" d="m7 21-4.3-4.3c-1-1-1-2.5 0-3.4l9.6-9.6c1-1 2.5-1 3.4 0l5.6 5.6c1 1 1 2.5 0 3.4L13 21"></path><path d="M22 21H7"></path><path d="m5 11 9 9"></path></svg><b>Clear</b>`;
 clearButton.classList.add("button-n");
+clearButton.onclick = () => {
+  localStorage.removeItem("clipBoard");
+  document.querySelectorAll(".card-grid-item .check-button").forEach((e) => {
+    e.innerHTML = outList;
+    e.setAttribute("data-is-in-deck", "false");
+  });
+};
 
 clipBoardBar.appendChild(copyButton);
 clipBoardBar.appendChild(clearButton);
