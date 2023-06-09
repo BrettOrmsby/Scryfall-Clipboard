8d55868b6ae8393df530db8a7f2ec59a5c001a3d
BrettOrmsby <brettwormsby@outlook.com>
2023-06-09 13:21:15 -0400

    feat: show icons and amount of cards in clipboard

--- main.js
+++ main.js
@@ -1,6 +1,59 @@
 const clipBoard = getClipBoard();
 const inList = `<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-check"><polyline points="20 6 9 17 4 12"></polyline></svg>`;
 const outList = `<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-plus"><line x1="12" x2="12" y1="5" y2="19"></line><line x1="5" x2="19" y1="12" y2="12"></line></svg>`;
+
+/*
+ * Menu functionality
+ */
+const reference = document.querySelector(".search-info");
+const clipBoardBar = document.createElement("div");
+clipBoardBar.classList.add("clipboard-bar");
+
+const copyButton = document.createElement("button");
+copyButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-paperclip"><path class="no-fill" d="m21.44 11.05-9.19 9.19a6 6 0 0 1-8.49-8.49l8.57-8.57A4 4 0 1 1 18 8.84l-8.59 8.57a2 2 0 0 1-2.83-2.83l8.49-8.48"></path></svg><b>Copy</b>`;
+copyButton.classList.add("button-n");
+
+let copyButtonClickTimeout;
+copyButton.onclick = () => {
+  const clipBoard = getClipBoard();
+  const clipBoardWithPrefixNumbers = clipBoard.map((e) => "1 " + e).join("\n");
+  navigator.clipboard.writeText(clipBoardWithPrefixNumbers);
+  copyButton.querySelector("b").innerText = "Copied";
+
+  clearTimeout(copyButtonClickTimeout);
+  copyButtonClickTimeout = setTimeout(
+    () => (copyButton.querySelector("b").innerText = "Copy"),
+    2000
+  );
+};
+
+const clearButton = document.createElement("button");
+clearButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-eraser"><path class="no-fill" d="m7 21-4.3-4.3c-1-1-1-2.5 0-3.4l9.6-9.6c1-1 2.5-1 3.4 0l5.6 5.6c1 1 1 2.5 0 3.4L13 21"></path><path d="M22 21H7"></path><path d="m5 11 9 9"></path></svg><b>Clear</b>`;
+clearButton.classList.add("button-n");
+
+clearButton.onclick = () => {
+  localStorage.removeItem("clipBoard");
+  document.querySelectorAll(".check-button").forEach((e) => {
+    e.innerHTML = outList;
+    e.setAttribute("data-is-in-deck", "false");
+  });
+  updateNumberInClipboard();
+};
+
+const numberInClipBoard = document.createElement("span");
+numberInClipBoard.classList.add("cards-in-deck");
+updateNumberInClipboard();
+
+clipBoardBar.appendChild(copyButton);
+clipBoardBar.appendChild(clearButton);
+clipBoardBar.appendChild(numberInClipBoard);
+
+insertAfter(clipBoardBar, reference);
+
+function insertAfter(newNode, existingNode) {
+  existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
+}
+
 /*
  * Button functionality
  */
@@ -35,43 +88,13 @@ document.querySelectorAll(".card-grid-item").forEach((e) => {
       button.innerHTML = outList;
       button.setAttribute("data-is-in-deck", "false");
     }
+    updateNumberInClipboard();
   });
 });
 
-/*
- * Menu functionality
- */
-const reference = document.querySelector(".search-info");
-const clipBoardBar = document.createElement("div");
-clipBoardBar.classList.add("clipboard-bar");
-
-const copyButton = document.createElement("button");
-copyButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-paperclip"><path class="no-fill" d="m21.44 11.05-9.19 9.19a6 6 0 0 1-8.49-8.49l8.57-8.57A4 4 0 1 1 18 8.84l-8.59 8.57a2 2 0 0 1-2.83-2.83l8.49-8.48"></path></svg><b>Copy</b>`;
-copyButton.classList.add("button-n");
-copyButton.onclick = () => {
+function updateNumberInClipboard() {
   const clipBoard = getClipBoard();
-  const clipBoardWithPrefixNumbers = clipBoard.map((e) => "1 " + e).join("\n");
-  navigator.clipboard.writeText(clipBoardWithPrefixNumbers);
-};
-
-const clearButton = document.createElement("button");
-clearButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-eraser"><path class="no-fill" d="m7 21-4.3-4.3c-1-1-1-2.5 0-3.4l9.6-9.6c1-1 2.5-1 3.4 0l5.6 5.6c1 1 1 2.5 0 3.4L13 21"></path><path d="M22 21H7"></path><path d="m5 11 9 9"></path></svg><b>Clear</b>`;
-clearButton.classList.add("button-n");
-clearButton.onclick = () => {
-  localStorage.removeItem("clipBoard");
-  document.querySelectorAll(".card-grid-item .check-button").forEach((e) => {
-    e.innerHTML = outList;
-    e.setAttribute("data-is-in-deck", "false");
-  });
-};
-
-clipBoardBar.appendChild(copyButton);
-clipBoardBar.appendChild(clearButton);
-
-insertAfter(clipBoardBar, reference);
-
-function insertAfter(newNode, existingNode) {
-  existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
+  numberInClipBoard.innerText = clipBoard.length + " cards in clipboard";
 }
 
 function getClipBoard() {
